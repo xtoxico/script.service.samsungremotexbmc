c24cb3588dfb43fcef7016648c79fa0138fdfb2d
Antonio Tirado Peña <yosoy@antoniotirado.com>
2014-01-09 20:11:16 +0100

    Versión 0.20
    Primera versión funcional

--- default.py
+++ default.py
@@ -1,3 +1,13 @@
+#!  /usr/bin/python
+#   Title: samsungremote.py
+#   Author: Asif Iqbal
+#   Date: 05APR2012
+#   Info: To send remote control commands to the Samsung tv over LAN
+#   TODO:
+ 
+import socket
+import base64
+import time, datetime
 import xbmc
 import xbmcaddon
 import xbmcgui
@@ -13,17 +23,31 @@ __icon__ = __addon__.getAddonInfo('icon')
 __ID__ = __addon__.getAddonInfo('id')
 __language__ = __addon__.getLocalizedString
 
+
+tvip = "192.168.1.250"
+#IP Address of TV
+myip = "192.168.1.50"
+#Used for the access control/validation, but not after that AFAIK
+mymac = "00-01-80-65-BE-A3"
+#What the iPhone app reports
+appstring = "iphone..iapp.samsung"
+#Might need changing to match your TV type
+tvappstring = "iphone.UE50ES5500.iapp.samsung"
+#What gets reported when it asks for permission
+remotename = "XBMC Samsung Remote by xtoxico"
+ 
+
+
 global g_jumpBackSecs
 g_jumpBackSecs = 0
 
+# Function to send keys
+def sendKey(skey, dataSock, appstring):
+  messagepart3 = chr(0x00) + chr(0x00) + chr(0x00) + chr(len(base64.b64encode(skey))) + chr(0x00) + base64.b64encode(skey);
+  part3 = chr(0x00) + chr(len(appstring)) + chr(0x00) + appstring + chr(len(messagepart3)) + chr(0x00) + messagepart3 dataSock.send(part3);
+
 def log(msg):
   xbmc.log("### [%s] - %s" % (__scriptname__,msg,),level=xbmc.LOGDEBUG )
-
-def netcat(hostname, port, content):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((hostname, port))
-    s.send(content)
-    s.shutdown(socket.SHUT_WR)    
 
 def getSetting(setting):
   return __addon__.getSetting(setting).strip()
@@ -36,21 +60,32 @@ class MyPlayer( xbmc.Player ):
     #log('MyPlayer - init')
   
   def onPlayBackStarted( self ):
-    netcat("192.168.1.200",1729,"set salon off")
-  
-  def onPlayBackPaused( self ):
-    netcat("192.168.1.200",1729,"set salon on")
-  
-  def onPlayBackResumed( self ):
-    netcat("192.168.1.200",1729,"set salon off")
-
-  def onPlayBackStopped( self ):
-    netcat("192.168.1.200",1729,"set salon on")    
-  
-  def onPlayBackEnded( self ):
-    netcat("192.168.1.200",1729,"set salon on")   
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((tvip, 55000))
+    ipencoded = base64.b64encode(myip)
+    macencoded = base64.b64encode(mymac)
+    messagepart1 = chr(0x64) + chr(0x00) + chr(len(ipencoded)) + chr(0x00) + ipencoded + chr(len(macencoded)) + chr(0x00) + macencoded + chr(len(base64.b64encode(remotename))) + chr(0x00) + base64.b64encode(remotename)
+    part1 = chr(0x00) + chr(len(appstring)) + chr(0x00) + appstring + chr(len(messagepart1)) + chr(0x00) + messagepart1
+    sock.send(part1)
+    messagepart2 = chr(0xc8) + chr(0x00)
+    part2 = chr(0x00) + chr(len(appstring)) + chr(0x00) + appstring + chr(len(messagepart2)) + chr(0x00) + messagepart2
+    sock.send(part2)
+    sendKey("KEY_HDMI1",sock,tvappstring)
+    time.sleep(1)
+    sock.close()
 
 player_monitor = MyPlayer()
 
 while not xbmc.abortRequested:
       xbmc.sleep(100)
+
+
+
+
+
+
+
+ 
+
+
+
